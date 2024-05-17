fd984d848f6571d75e4563500f3f3bf2e27ffb51
VladSkopenko <skopirka2k17@gmail.com>
2024-05-17 21:08:15 +0300

    + addition and subsraction matrix

--- matrix.py
+++ matrix.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-object_matrix = np.array([
-    [1, 2, 3],
-    [4, 5, 6]
-], dtype=int)
+object_matrix = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
 
 # Створення матриці  - це массив з массивами.
 # .shape  показує розмірність матриці
@@ -13,4 +10,12 @@ object_matrix = np.array([
 first_value, second_value = object_matrix.shape
 
 print(f"{first_value} - кількість рядків(m)\n{second_value} - кількість стопців(n)")
+print(object_matrix.size)
 
+a = np.array([[1, 2, 3], [0, 1, 2]])
+
+b = np.array([[1, 2], [0, 4], [-1, 0]])
+
+
+c = np.dot(a, b)
+print(c)
