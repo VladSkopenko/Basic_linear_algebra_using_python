266d0fb67dbd39549e270cd0a4436259b944bef4
VladSkopenko <skopirka2k17@gmail.com>
2024-05-18 19:25:52 +0300

    flatten and reshape:

--- change_chape_matrix.py
+++ change_chape_matrix.py
@@ -3,6 +3,10 @@ import numpy as np
 a = np.array([[1, 2, 3],
               [4, 5, 6]])
 print(a)
-print("Розмірність матриці")
+print("Розмірність матриці reshape()")
 b = a.reshape((3, 2))
+print(b)
+print("Матриця плоска flatten()")
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = a.flatten()
 print(b)
