88562f9291761f69084bb033eb4a0d431bfc517b
VladSkopenko <skopirka2k17@gmail.com>
2024-05-19 12:47:48 +0300

    update identity matrix

---
+++ identity matrix.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+print(
+    "Identity matrix - матриця ідентичності, створюється методом eye а параметр k це діагональне зміщення"
+)
+a = np.eye(5, k=0, dtype=float)
+print(a)
