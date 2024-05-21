defa3984d9e9e04cde6cb30958582b6efd63299d
VladSkopenko <skopirka2k17@gmail.com>
2024-05-21 20:33:50 +0300

    wazure

--- mathlotlib.py
+++ mathlotlib.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-date = pd.date_range(start='2021-09-01', freq='D', periods=8)
-plt.plot(date, [23, 17, 17, 16, 15, 14, 17, 20])
+date = pd.date_range(start='2024-05-21', freq='D', periods=7)
+plt.plot(date, [22, 23, 24, 25, 24, 16, 20], color='red', marker='o')
 plt.show()
+# Графік погоди в Одесі починаючи з 2024-05-21
